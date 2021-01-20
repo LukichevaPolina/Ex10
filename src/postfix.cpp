61d666aec2e81ba07ef805ab4008ee49d27b027f
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-01-20 11:23:11 +0300

    fix style

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -8,48 +8,38 @@ std::string infix2postfix(std::string infix) {
   while (i < infix.size()) {
     if (infix[i] == '(')
       stack.push(infix[i++]);
-    else {
-      if (infix[i] == ')') {
-        while (!stack.isEmpty() && stack.get() != '(') {
+    else if (infix[i] == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        postfix += stack.pop();
+        postfix += ' ';
+      }
+      if (stack.get() == '(') stack.pop();
+      ++i;
+    } else if (infix[i] == '*' || infix[i] == '/') {
+      if (stack.get() == '/' || stack.get() == '*' || stack.get() == ')') {
+        while (!stack.isEmpty() && stack.get() != '(' && stack.get() != '+' &&
+               stack.get() != '-') {
           postfix += stack.pop();
           postfix += ' ';
         }
         if (stack.get() == '(') stack.pop();
-        ++i;
-      } else {
-        if (infix[i] == '*' || infix[i] == '/') {
-          if (stack.get() == '/' || stack.get() == '*' || stack.get() == ')') {
-            while (!stack.isEmpty() && stack.get() != '(' &&
-                   stack.get() != '+' && stack.get() != '-') {
-              postfix += stack.pop();
-              postfix += ' ';
-            }
-            if (stack.get() == '(') stack.pop();
-            stack.push(infix[i++]);
-          } else
-            stack.push(infix[i++]);
-        }
-
-        else {
-          if (infix[i] == '+' || infix[i] == '-') {
-            while (!stack.isEmpty() && stack.get() != '(') {
-              postfix += stack.pop();
-              postfix += ' ';
-            }
-            if (stack.get() == '(') stack.pop();
-            stack.push(infix[i++]);
-          } else {
-            if (infix[i] != ' ') {
-              while (infix[i] >= '0' && infix[i] <= '9' || infix[i] == '.') {
-                postfix += infix[i++];
-              }
-              postfix += ' ';
-            } else
-              i++;
-          }
-        }
+        stack.push(infix[i++]);
+      } else
+        stack.push(infix[i++]);
+    } else if (infix[i] == '+' || infix[i] == '-') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        postfix += stack.pop();
+        postfix += ' ';
       }
-    }
+      if (stack.get() == '(') stack.pop();
+      stack.push(infix[i++]);
+    } else if (infix[i] != ' ') {
+      while (infix[i] >= '0' && infix[i] <= '9' || infix[i] == '.') {
+        postfix += infix[i++];
+      }
+      postfix += ' ';
+    } else
+      i++;
   }
   while (!stack.isEmpty()) {
     postfix += stack.pop();
