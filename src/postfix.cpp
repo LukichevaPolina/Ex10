afcf08b54614557ebccd83f873329b34c0757885
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-01-20 10:59:25 +0300

    fix style

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -5,8 +5,7 @@ std::string infix2postfix(std::string infix) {
   std::string postfix;
   Stack<char> stack(200);
   int i = 0;
-  while ( i < infix.size()) {
-    //если скобка, то положить ее в стек
+  while (i < infix.size()) {
     if (infix[i] == '(')
       stack.push(infix[i++]);
     else if (infix[i] == ')') {
@@ -14,41 +13,33 @@ std::string infix2postfix(std::string infix) {
         postfix += stack.pop();
         postfix += ' ';
       }
-      if (stack.get() == '(')
-        stack.pop();
+      if (stack.get() == '(') stack.pop();
       ++i;
-    }
-    //если умножение
-    else if (infix[i] == '*' || infix[i] == '/') {
-      //очищаем стек до скобки или пустоты если лежит 
+    } else if (infix[i] == '*' || infix[i] == '/') {
       if (stack.get() == '/' || stack.get() == '*' || stack.get() == ')') {
-        while (!stack.isEmpty() && stack.get() != '(' && stack.get() != '+' && stack.get() != '-') {
+        while (!stack.isEmpty() && stack.get() != '(' && stack.get() != '+' &&
+               stack.get() != '-') {
           postfix += stack.pop();
           postfix += ' ';
         }
-        if (stack.get() == '(')
-          stack.pop();
+        if (stack.get() == '(') stack.pop();
         stack.push(infix[i++]);
-      }
-      else
+      } else
         stack.push(infix[i++]);
-    }
-    else if (infix[i] == '+' || infix[i] == '-') {
+    } else if (infix[i] == '+' || infix[i] == '-') {
       while (!stack.isEmpty() && stack.get() != '(') {
         postfix += stack.pop();
         postfix += ' ';
       }
-      if (stack.get() == '(')
-        stack.pop();
+      if (stack.get() == '(') stack.pop();
       stack.push(infix[i++]);
-    }
-    else if (infix[i] != ' ') {
+    } else if (infix[i] != ' ') {
       while (infix[i] >= '0' && infix[i] <= '9' || infix[i] == '.') {
         postfix += infix[i++];
       }
       postfix += ' ';
-    }
-    else (i++);
+    } else
+      i++;
   }
   while (!stack.isEmpty()) {
     postfix += stack.pop();
